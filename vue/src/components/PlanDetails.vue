44714585dc4d4d76a8f72861ac91493d8358c9d2
tjordanv <tylervicari@gmail.com>
2021-09-21 21:21:48 -0400

    connected to rapid API to pull house listing prices

--- vue/src/components/PlanDetails.vue
+++ vue/src/components/PlanDetails.vue
@@ -5,10 +5,11 @@
         Plan Name:
         <input type="text" class='plan-name' v-model="planBase.name" />
         <label for="house-types" class='house-types'>Select House Type:</label>
-        <select name="house-types" v-model="planBase.houseType">
+        <select name="house-types" v-model="planBase.houseType" required>
           <option value="single">Single-Family</option>
           <option value="detached">Detached</option>
           <option value="townhouse">Townhouse</option>
+          <option value="land">Empty Lot</option>
         </select>
         <!-- <label for="region-types">Select Region:</label>
         <select name="region-types" v-model="planBase.region">
@@ -19,25 +20,34 @@
           <option value="West">West</option>
         </select> -->
         <label for="City" class='city'>Select City:</label>
-        <select name="city" v-model="planBase.city">
+        <select name="city" v-model="planBase.city" required>
           <option value="Cleveland">Cleveland</option>
           <option value="Cincinnati">Cincinnati</option>
           <option value="Columbus">Columbus</option>
           <option value="Toledo">Toledo</option>
         </select>
         <label for="State" class='state'>Select State:</label>
-        <select name="state" v-model="planBase.state">
+        <select name="state" v-model="planBase.state" required>
           <option value="Ohio">OH</option>
         </select>
-        <label for="square-foot" class='size'>Select Square Footage:</label>
-        <select name="square-foot" v-model="planBase.size">
+        <label for="square-foot" class='size'>Select Minimum Square Footage:</label>
+        <select v-if="planBase.houseType !== 'land'" name="square-foot" v-model="planBase.size" required>
           <option value="1000">1000</option>
           <option value="1500">1500</option>
           <option value="2000">2000</option>
           <option value="2500">2500</option>
         </select>
+        <select v-if="planBase.houseType === 'land'" name="square-foot" v-model="planBase.size" required>
+          <option value="2000">2000</option>
+          <option value="4000">4000</option>
+          <option value="5000">5000</option>
+          <option value="7500">7500</option>
+        </select>
         <button>Next</button>
       </form>
+      <div v-if="priceCeiling !== -1">
+        <h3>${{priceFloor}} - ${{priceCeiling}}</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -54,29 +64,37 @@ export default {
         state: "",
         size: ""
       },
+      priceFloor: -1,
+      priceCeiling: -1
     };
   },
   methods: {
     saveBase() {
-      const newPlan = {
-        name:this.planBase.name,
-        houseType: this.planBase.houseType,
-        city:this.planBase.city,
-        state: this.planBase.state,
-        size: this.planBase.size
+      let options = {};
+      this.priceFloor = -1;
+      this.priceCeiling = -1;
+      if (this.planBase.houseType !== "land") {
+        options = planService.setPropertyInfo({offset: '0', limit: '10', sort: 'newest', state_code: this.planBase.state, 
+        city: this.planBase.city, property_type: this.planBase.houseType, home_size_min: this.planBase.size});
+      } else {
+        options = planService.setPropertyInfo({offset: '0', limit: '10', sort: 'newest', state_code: this.planBase.state, 
+        city: this.planBase.city, property_type: this.planBase.houseType, lot_size_min: this.planBase.size});
       }
-      console.log(newPlan)
-      planService
-      .create(newPlan)
-      .then(
-        this.planBase = {
-        name: '',
-        houseType: '',
-        city: '',
-        state:'',
-        size: ''},
-        this.$router.push("/plan-builder")
-      )
+      planService.getProperties(options).then(
+        (response) => {
+          let results = response.data.data.home_search.results;
+          results.forEach(
+            (element) => {
+              if (element.list_price > this.priceCeiling) {
+                this.priceCeiling = element.list_price;
+              }
+              if (element.list_price < this.priceFloor || this.priceFloor === -1) {
+                this.priceFloor = element.list_price;
+              } 
+              console.log(element.list_price);
+          });
+        }
+      );
     },
   },
 };
--- vue/src/services/PlanService.js
+++ vue/src/services/PlanService.js
@@ -1,8 +1,26 @@
 import axios from 'axios';
 
 export default{
-    create(planBase) {
-        return axios.post('/newPlan', planBase);
-      },
-    
+  create(planBase) {
+      return axios.post('/newPlan', planBase);
+    },
+  setPropertyInfo(info) {
+    return {
+      method: 'GET',
+      url: 'https://us-real-estate.p.rapidapi.com/v2/for-sale',
+      params: info,
+      headers: {
+      'x-rapidapi-host': 'us-real-estate.p.rapidapi.com',
+      'x-rapidapi-key': 'babb77fc36msh84d572d81feb391p146b30jsn26eca0abd8f0'
+      }
+    };
+  },  
+  getProperties(options) {
+    return axios.request(options);
+    // .then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.error(error);
+    // });
+  }
 }
