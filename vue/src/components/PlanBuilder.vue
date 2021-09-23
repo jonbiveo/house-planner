da555ed1d8d9a4dff4a8781f106425f7d69a7b0c
Vellencia Knightly <vellencia.rolle@gmail.com>
2021-09-23 10:15:03 -0400

    'Add Bedroom' button and function

--- vue/src/components/PlanBuilder.vue
+++ vue/src/components/PlanBuilder.vue
@@ -14,18 +14,19 @@
              <option value = "10x10">10x10</option>
              <option value = "20x20">20x20</option>
          </select><br>
-         <label for="bedroom" class="bedroom"> Select Number of Bedrooms: </label>
+         <!-- <label for="bedroom" class="bedroom"> Select Number of Bedrooms: </label>
          <select name ="bedroom" v-model="floorPlan.bedrooms" required>
              <option value = "1">1</option>
              <option value = "2">2</option>
              <option value = "3">3</option>
              <option value = "4">4</option>
-         </select><br>
-          <label for="bedroom-size" class="bedroom-size"> Select Bedroom Size:</label>
+         </select><br> -->
+         <button id="add-room" v-on:click.prevent="addRoom">Add Bedroom</button><br>
+          <!-- <label for="bedroom-size" class="bedroom-size"> Select Bedroom Size:</label>
          <select name ="bedroom-size">
              <option value = "masterBed">Master Bedroom: 14x16</option>
              <option value = "standardBed">Standard Bedroom: 11x12</option>
-         </select><br>
+         </select><br> -->
          <label for="bathroom" class="bathroom"> Select Number of Bathrooms:</label>
          <select name ="bathroom" v-model="floorPlan.bathrooms" required>
              <option value = "1">1</option>
@@ -53,11 +54,32 @@ export default {
                 kitchen: "",
                 bedrooms:[],
                 bathrooms:[]
-            }
-
+            },
+            bedroomCount:0,
+            bathroomCount:0,
             }   
     },
     methods: {
+      addBedroom(){
+        this.bedroomCount++
+        const label = document.createElement("label")
+        const roomSelection = document.createElement("select")
+        const lineBreak = document.createElement("br")
+        roomSelection.className="new-bedroom"
+
+        label.innerText="Bedroom "+this.bedroomCount+": "
+
+        roomSelection.innerHTML="<option value='5x5'>5x5</option>"+
+                                "<option value='7x7'>7x7</option>"+
+                                "<option value='10x10'>10x10</option>"
+        
+        const roomToAdd = document.getElementById("add-room")
+        
+        roomToAdd.parentNode.insertBefore(label, roomToAdd)
+        roomToAdd.parentNode.insertBefore(roomSelection, roomToAdd)
+        roomToAdd.parentNode.insertBefore(lineBreak, roomToAdd)
+        console.log(this.floorPlan)
+      }
     }
 
 }
