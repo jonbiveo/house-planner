54cc0a3c8b228e4098bdde19e643b0f927334c34
Molly Akers <mollyakers@gmail.com>
2021-09-24 07:28:54 -0500

    PlanBuilder updated bedroom/bathroom btn and css

--- vue/src/components/PlanBuilder.vue
+++ vue/src/components/PlanBuilder.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='main'>
   <div class='plan-builder'>
-      <form > <!--submit? method to save? -->
+      <form class="form-builder"> <!--submit? method to save? -->
           <h2>Floors</h2>
           <label for="floors" class="floors"> Select Number of Floors: </label>
           <select name="floors" v-model="floorPlan.floors" required>
@@ -42,11 +42,11 @@
              <option value = "4">4</option>
          </select><br> -->
     
-          <label for="bedroom-size" class="bedroom-size"> Select Bedroom Size:</label>
+          <!-- <label for="bedroom-size" class="bedroom-size"> Select Bedroom Size:</label>
          <select name ="bedroom-size" v-model="floorPlan.bedrooms" required>
              <option value = "14x16">Master Bedroom: 14x16</option>
              <option value = "11x12">Standard Bedroom: 11x12</option>
-         </select><br>
+         </select><br> -->
         <button id="add-room" v-on:click.prevent="addBedroom">Add Bedroom</button><br>
          <!-- <label for="bathroom" class="bathroom"> Select Number of Bathrooms:</label> -->
          <!-- <select name ="bathroom" v-model="floorPlan.bathrooms" required>
@@ -55,15 +55,15 @@
              <option value = "3">3</option>
          </select><br> -->
         
-        <label for="bathroom-size" class="bathroom-size"> Select Bathroom Size: </label>
-         <select name ="bathroom-size" v-model="floorPlan.bathrooms" required>
+        <!-- <label for="bathroom-size" class="bathroom-size"> Select Bathroom Size: </label> -->
+         <!-- <select name ="bathroom-size" v-model="floorPlan.bathrooms" required>
              <option value = "6x12">Master Bathroom: 6x12</option>
              <option value = "5x8">Standard Bathroom: 5x8</option>
              <option value = "4x6">Half Bath: 4x6</option>
-         </select><br>
+         </select><br> -->
 
           <button id="add-bathroom" v-on:click.prevent="addBathroom">Add Bathroom</button><br>
-         <button>Next</button>
+          <button class="next-btn" v-on:click.prevent="saveRooms">Next</button>
          </form>
   </div> 
   </div>
@@ -83,31 +83,39 @@ export default {
             },
             bedroomCount:0,
             bathroomCount:0,
+            bedrooms: [],
+            bathrooms: []
             }   
     },
     methods: {
+      saveRooms(){
+          this.floorPlan.bedrooms = this.bedrooms;
+          this.floorPlan.bathrooms = this.bathrooms;
+      },
       addBedroom(){
         this.bedroomCount++
         const label = document.createElement("label");
         const roomSelection = document.createElement("select");
         const lineBreak = document.createElement("br");
         roomSelection.className="new-bedroom";
-        label.innerText="Bedroom "+this.bedroomCount+": "
+        label.innerText="Bedroom "+this.bedroomCount+": ";
 
 
-
-        roomSelection.innerHTML="<option value = '14x16'> Master Bedroom: 14x16 </option>"+
+        roomSelection.innerHTML= "<option value = ''/>" + "<option value = '14x16'> Master Bedroom: 14x16 </option>"+
                                 "<option value = '11x12'>Standard Bedroom: 11x12</option>"+
                                 "<option value='10x10'>Smalll Bedroom: 10x10</option>"
         
-        const roomToAdd = document.getElementById("add-room")
+        const roomToAdd = document.getElementById("add-room");
         
-        roomToAdd.parentNode.insertBefore(label, roomToAdd)
-        roomToAdd.parentNode.insertBefore(roomSelection, roomToAdd)
+        roomToAdd.parentNode.insertBefore(label, roomToAdd);
+        roomToAdd.parentNode.insertBefore(roomSelection, roomToAdd);
         roomToAdd.parentNode.insertBefore(lineBreak, roomToAdd);
         console.log(this.floorPlan);
-        console.log(roomSelection.value);
-        this.bedrooms.push(roomSelection.value);
+        roomSelection.addEventListener("change", (event) => {
+            console.log(event.target.value);
+            this.bedrooms.push(event.target.value);
+        });
+
       },
       addBathroom(){
         this.bathroomCount++
@@ -118,16 +126,21 @@ export default {
 
         label.innerText="Bathroom "+this.bathroomCount+": "
 
-        roomSelection.innerHTML="<option value = 'masterBath'>Master Bathroom: 6x12</option>"+
+        roomSelection.innerHTML="<option value = ''/>" + "<option value = 'masterBath'>Master Bathroom: 6x12</option>"+
                                 "<option value = 'standardBath'>Standard Bathroom: 5x8</option>"+
                                 "<option value = 'halfBath'>Half Bath: 4x6</option>"
         
         const roomToAdd = document.getElementById("add-bathroom")
         
-        roomToAdd.parentNode.insertBefore(label, roomToAdd)
-        roomToAdd.parentNode.insertBefore(roomSelection, roomToAdd)
-        roomToAdd.parentNode.insertBefore(lineBreak, roomToAdd)
-        console.log(this.floorPlan)
+        roomToAdd.parentNode.insertBefore(label, roomToAdd);
+        roomToAdd.parentNode.insertBefore(roomSelection, roomToAdd);
+        roomToAdd.parentNode.insertBefore(lineBreak, roomToAdd);
+        console.log(this.floorPlan);
+        roomSelection.addEventListener("change", (event) => {
+            console.log(event.target.value);
+            this.bathrooms.push(event.target.value);
+        })
+
       }
     
     }
@@ -136,14 +149,48 @@ export default {
 
 <style scoped>
 .main{
- text-align: center;
- background: #264653;
- color: white;
+  display: grid;
+  height: 75vh;
+  margin-top: 50px;
+  grid-template-columns: 1fr;
+  grid-template-areas: 
+  "form";
+  justify-items: center;
+  align-items: center;
+  color: white;
+}
+.plan-builder{
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-areas: 
+  "form";
+  justify-items: center;
+  align-items: center;
+  width: 60vw;
+  background-color: #264653;
+  border-radius: 10px;
+  padding: 10px;
 }
 h1{
     text-emphasis: bold;
 }
-select{
-    margin: 0.75em;
+
+.form-builder{
+    margin-top: 15px;
+    grid-area: form;  
+    display: grid;
+    justify-content: center;
+    width: 55%;
+    padding: 10px;
+}
+
+.next-btn{
+  margin: 15px auto;
+  min-width: 150px;
+  height: 35px;
+  font-size: 16pt;
+  border-radius: 15px;
+  border: none;
+  background-color: #E76F51;
 }
 </style>
